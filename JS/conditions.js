fa4741ff83fedc59c7fdbcefabfba400ad2a1c29
killerkoli <gpraveen29041996@gmail.com>
2020-06-06 18:00:37 +0530

    1 file modified

--- JS/conditions.js
+++ JS/conditions.js
@@ -1,3 +1,5 @@
+// =======If else Condition ============///
+
 // If the condition cuurent hours greater than 1AM and less than 12PM then say Good morning on console
 
 //  Else If the condition cuurent hours greater than 13PM and less than 18PM then say Good afternoon on console
@@ -12,4 +14,28 @@ if (hours.getHours() >= 1 && hours.getHours() <= 12) {
   console.log("good evening" + " " + " cuurent time is", hours);
 } else {
   console.log("good Mooringin" + " " + " cuurent time is", hours);
+}
+
+
+// ===========Switch Case ==========//
+
+let employee = 'webDesigner' ;
+
+switch (employee) {
+  case 'webDesigner':
+  console.log('Front End Developer');
+  break;
+
+  case 'webDeveloper':
+  console.log('Back End Developer');
+  break;
+
+  default : console.log('Full stack Developer');
+}
+
+// =========For Loop ============//
+
+for (let i = 1; i <= 5; i++ ){
+   console.log(i)
+
 }
