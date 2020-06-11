521eeb6713560f9ae8ca4b2579ca78345ba6e6a2
killerkoli <gpraveen29041996@gmail.com>
2020-06-12 00:44:57 +0530

    2 files modified

--- JS/conditions.js
+++ JS/conditions.js
@@ -49,3 +49,56 @@ while (i <= 5) {
     console.log(i);
   }
 }
+
+// ==========For in=============//
+
+const products = {
+  name: "google",
+  items: "youtube"
+};
+for (let index in products) {
+  console.log(index, products[index]);
+}
+
+// ==========For of=============//
+
+const empDet = [
+  {
+    name: "Praveen",
+    id: "1813677",
+    role: "Designer"
+  },
+  {
+    name: "Solomon",
+    id: "1813678",
+    role: "Developer"
+  }
+];
+for (let emp of empDet) {
+  console.log(emp);
+}
+
+// ======Max ======//
+// Returns the highest value
+let number = max(5, 3);
+console.log("Max number is", number);
+
+function max(a, b) {
+  return a > b ? a : b;
+}
+
+// ===fizzBuzz=====//
+// if the typeof number is divisible by 2 then return "designer"
+// if the typeof number is divisible by 4 then return "webDesigner"
+// if the typeof number is divisible by 4,2 then return "Front end Developer"
+// if the typeof number is Not a number  then return "NaN"
+
+const status = fizzBuzz();
+console.log("Input type is", status);
+function fizzBuzz(input) {
+  if (typeof input !== "number") return "Not a number";
+  if (input % 2 === 0 && input % 4 === 0) return "Front End Developer";
+  if (input % 2 !== 0 && input % 4 !== 0) return "Back End Developer";
+  if (input % 2 === 0) return "Designer";
+  if (input % 4 === 0) return "Web Designer";
+}
